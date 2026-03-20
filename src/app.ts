a3f25675cc0592cb29840dec1d0bac02356002cc
LMCuong2K1 <lmc22112001@gmail.com>
2026-03-21 00:21:31 +0700

    add dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 // const express = require('express');
 import express from 'express';
 const app = express();
-const port = 8080;
+import 'dotenv/config';
+const port = process.env.PORT||8080;
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 app.listen(port, () => {
     console.log("My app running on port: ", port);
 })
