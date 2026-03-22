b825f09eba09ce8f43bd703c0913aa25ad7c42f4
LMCuong2K1 <lmc22112001@gmail.com>
2026-03-22 18:37:07 +0700

    add createUserPage

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import express from 'express';
 import 'dotenv/config';
 import webRoutes from '../src/routes/web';
 const app = express();
-<<<<<<< HEAD
 const port = process.env.PORT || 8080;
 //config views 
 app.set('view engine', 'ejs');
@@ -19,14 +18,6 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.render('home.ejs');
 })
-=======
-import 'dotenv/config';
-const port = process.env.PORT||8080;
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
->>>>>>> a3f25675cc0592cb29840dec1d0bac02356002cc
 app.listen(port, () => {
     console.log("My app running on port: ", port);
 })
--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,9 +1,10 @@
 import express, { Express } from 'express';
-import { getHomePage } from '../controllers/user.controller';
+import { getHomePage,getCreateUserPage } from '../controllers/user.controller';
 const router = express.Router();
 
 const webRoutes = (app: Express) => {
     router.get('/', getHomePage);
+    router.get('/create-user',getCreateUserPage);
     app.use('/', router);
 }
 export default webRoutes;
